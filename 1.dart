08adf3679ae3ed1389289039fcbea688fe2ae032
coder-aastha <aasthadahal312@gmail.com>
2024-11-18 06:42:52 +0545

    Returns the multiplication of default number

--- 1.dart
+++ 1.dart
@@ -1,10 +1,10 @@
-//Returns the length of the string
+//Returns the multiplication of default number
 
 int multiplyNumbers(int a, [int b = 2]) {
   return a * b;
 }
 
 void main() {
-  print(multiplyNumbers(5));   
-  print(multiplyNumbers(5, 3)); 
+  print(multiplyNumbers(5));
+  print(multiplyNumbers(5, 3));
 }
