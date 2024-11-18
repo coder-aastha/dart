0c5b978a0767f7dfc640742fe12a4157a615f596
coder-aastha <aasthadahal312@gmail.com>
2024-11-18 06:41:27 +0545

    Returns the length of the string

---
+++ 2.dart
@@ -0,0 +1,10 @@
+//Return the Length of a String
+
+int stringLength(String str) {
+  return str.length;
+}
+
+void main() {
+  String myString = "Hello, Dart!";
+  print("The length of the string is: ${stringLength(myString)}");
+}
